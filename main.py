e75e54f9ef7696d961004662ad800a254f5f663a
floh0 <florian.dasilva@ensiie.fr>
2019-08-24 19:13:48 +0200

    handle comments and combination keywords (union,except)

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ reserved = {
     'cast': 'CAST',
     'concat': 'CONCAT',
     'union': 'UNION',
-    'distinct': 'DISTINCT',
     'except': 'EXCEPT',
     'between': 'BETWEEN',
     'asc': 'ASC',
@@ -92,7 +91,7 @@ def t_LABEL(t):
      return t
 
 def t_COMMENT_ALONE(t):
-    r'(^|(?<=\n))\s*--[^\n]*'
+    r'(^|(?<=\n))--[^\n]*'
     return t
 
 def t_COMMENT(t):
@@ -100,7 +99,7 @@ def t_COMMENT(t):
     return t
 
 
-t_ignore = " \t\n"
+t_ignore = ' \t\n'
 
 def t_error(t):
     print(f"Illegal value '{t.value}'")
@@ -108,26 +107,53 @@ def t_error(t):
 
 lex.lex()
 
-numeric_regex = re.compile(r'^[\s\d\n\t\.\+\-\*\/\^\%\&\|\(\)\=\<\>\!\~\,\$]*$')
+numeric_regex = re.compile(r'^[\s\d\.\+\-\*\/\^\%\&\|\(\)\=\<\>\!\~\,\$]*$')
 is_numeric_expression = lambda x: numeric_regex.match(x)
 
+empty_line_regex = re.compile(r'\n\s*\n')
+spaces_start_regex = re.compile(r'^[ ]+', re.MULTILINE)
+comments_regex = re.compile(r'--\s*')
+comments_alone_regex = re.compile(r'^\s*--', re.MULTILINE)
+remove_useless_whitespaces = lambda x: comments_alone_regex.sub('--', comments_regex.sub('-- ', spaces_start_regex.sub('', empty_line_regex.sub('\n', x)))).strip()
+
+# ============================================================
+
+def p_formatted_query(p):
+    'formatted_query : query'
+    p[0] = remove_useless_whitespaces(p[1])
+
+# ============================================================
 
 def p_query(p):
-    'query : expr_definition'
+    'query : subquerry'
     p[0] = p[1]
 
 def p_query_with_semicolon(p):
-    'query : expr_definition SEMICOLON'
+    'query : query semicolon'
+    p[0] = f"{p[1]}{p[2]}"
+
+def p_query_with_comment(p):
+    'query : comment query'
     p[0] = f"{p[1]}{p[2]}"
 
 # ============================================================
 
-def p_subquerry_select(p):
-    'subquerry : select_block'
+def p_subquerry(p):
+    'subquerry : expr_definition'
     p[0] = p[1]
 
-def p_subquerry_more(p):
-    'subquerry : select_block additional_block_list'
+def p_subquerry_combined(p):
+    'subquerry : subquerry combine_keyword subquerry'
+    p[0] = f"{p[1]}\n{p[2]}\n{p[3]}"
+
+# ============================================================
+
+def p_select_full_select(p):
+    'select_full : select_block'
+    p[0] = p[1]
+
+def p_select_full_more(p):
+    'select_full : select_block additional_block_list'
     p[0] = f"{p[1]}\n{p[2]}"
 
 # ============================================================
@@ -150,6 +176,22 @@ def p_additional_block(p):
 
 # ============================================================
 
+def p_combine_keyword_composed(p):
+    '''
+    combine_keyword : union all
+                    | union distinct
+    '''
+    p[0] = f"{p[1]} {p[2]}"
+
+def p_combine_keyword_alone(p):
+    '''
+    combine_keyword : union
+                    | except
+    '''
+    p[0] = p[1]
+
+# ============================================================
+
 def p_select_block(p):
     'select_block : select_keyword select_clause'
     p[2] = p[2].replace('\n','\n\t')
@@ -157,22 +199,22 @@ def p_select_block(p):
 
 def p_keyword_block(p):
     '''
-    keyword_block : FROM clause
-                  | WHERE clause
-                  | LIMIT clause
-                  | HAVING clause
+    keyword_block : from clause
+                  | where clause
+                  | limit clause
+                  | having clause
     '''
-    p[0] = f"{p[1].upper()} {p[2]}"
+    p[0] = f"{p[1]} {p[2]}"
 
 def p_by_block(p):
     '''
-    by_block : GROUP BY clause
-             | ORDER BY clause
-             | CLUSTER BY clause
-             | DISTRIBUTE BY clause
-             | SORT BY clause
+    by_block : group by clause
+             | order by clause
+             | cluster by clause
+             | distribute by clause
+             | sort by clause
     '''
-    p[0] = f"{p[1].upper()} {p[2].upper()} {p[3]}" 
+    p[0] = f"{p[1]} {p[2]} {p[3]}" 
 
 # ============================================================
 
@@ -185,30 +227,30 @@ def p_clause(p):
 # ============================================================
 
 def p_join_block_on(p):
-    'join_block : join_expression clause ON expr_list'
+    'join_block : join_expression clause on expr_list'
     p[4] = p[4].replace('\n','\n\t')
-    p[0] = f"{p[1]} {p[2]}\n\t{p[3].upper()} {p[4]}"
+    p[0] = f"{p[1]} {p[2]}\n\t{p[3]} {p[4]}"
 
 def p_join_block_alone(p):
     'join_block : join_expression clause'
     p[0] = f"{p[1]} {p[2]}"
 
 def p_join_expression(p):
-    'join_expression : join_prefix_list JOIN'
-    p[0] = f"{p[1]} {p[2].upper()}"
+    'join_expression : join_prefix_list join'
+    p[0] = f"{p[1]} {p[2]}"
 
 def p_join_prefix(p):
     '''
-    join_prefix : INNER
-                | OUTER
-                | LEFT
-                | RIGHT
-                | FULL
-                | SEMI
-                | CROSS
-                | NATURAL
+    join_prefix : inner
+                | outer
+                | left
+                | right
+                | full
+                | semi
+                | cross
+                | natural
     '''
-    p[0] = p[1].upper()
+    p[0] = p[1]
 
 
 def p_join_prefix_list_next(p):
@@ -222,20 +264,20 @@ def p_join_prefix_list_end(p):
 # ============================================================
 
 def p_select_keyword_alone(p):
-    'select_keyword : SELECT'
-    p[0] = p[1].upper()
+    'select_keyword : select'
+    p[0] = p[1]
 
 def p_select_keyword_enriched(p):
     '''
-    select_keyword : SELECT DISTINCT
-                   | SELECT ALL
+    select_keyword : select distinct
+                   | select all
     '''
-    p[0] = f"{p[1].upper()} {p[2].upper()}"
+    p[0] = f"{p[1]} {p[2]}"
 
 # ============================================================
 
 def p_select_clause_next(p):
-    'select_clause : expr COMMA select_clause'
+    'select_clause : expr comma select_clause'
     p[0] = f"{p[1]}{p[2]}\n{p[3]}"
 
 def p_select_clause_end(p):
@@ -245,9 +287,9 @@ def p_select_clause_end(p):
 # ============================================================
 
 def p_case_when(p):
-    'case_when : CASE case_when_clause_list END'
+    'case_when : case case_when_clause_list end'
     p[2] = p[2].replace('\n','\n\t')
-    p[0] = f"{p[1].upper()}\n\t{p[2]}\n{p[3].upper()}"
+    p[0] = f"{p[1]}\n\t{p[2]}\n{p[3]}"
 
 def p_case_when_clause_list_next(p):
     'case_when_clause_list : case_when_clause case_when_clause_list'
@@ -258,13 +300,13 @@ def p_case_when_clause_list_end(p):
     p[0] = p[1]
 
 def p_case_when_clause_if(p):
-    'case_when_clause : WHEN expr THEN expr'
+    'case_when_clause : when expr then expr'
     p[2] = p[2].replace('\n',' ').replace('\t','')
-    p[0] = f"{p[1].upper()} {p[2]} {p[3].upper()} {p[4]}"
+    p[0] = f"{p[1]} {p[2]} {p[3]} {p[4]}"
 
 def p_case_when_clause_else(p):
-    'case_when_clause : ELSE expr'
-    p[0] = f"{p[1].upper()} {p[2]}"
+    'case_when_clause : else expr'
+    p[0] = f"{p[1]} {p[2]}"
 
 # ============================================================
 
@@ -284,72 +326,72 @@ def p_expr_definition_list_end(p):
     p[0] = p[1]
 
 def p_expr_definition_list_prefix(p):
-    'expr_definition_list : NOT expr_definition_list'
+    'expr_definition_list : not expr_definition_list'
     if len(p[1]) == 1:
         p[0] = f"{p[1]}{p[2]}"
     else:
-        p[0] = f"{p[1].upper()} {p[2]}"
+        p[0] = f"{p[1]} {p[2]}"
 
 def p_expr_definition_list_infix(p):
     '''
-    expr_definition_list : expr_definition COMPARISON expr_definition_list
-                         | expr_definition SYMBOL expr_definition_list
-                         | expr_definition AS expr_definition_list
-                         | expr_definition IS expr_definition_list
-                         | expr_definition IN expr_definition_list
+    expr_definition_list : expr_definition comparison expr_definition_list
+                         | expr_definition symbol expr_definition_list
+                         | expr_definition as expr_definition_list
+                         | expr_definition is expr_definition_list
+                         | expr_definition in expr_definition_list
     '''
-    p[0] = f"{p[1]} {p[2].upper()} {p[3]}"
+    p[0] = f"{p[1]} {p[2]} {p[3]}"
 
 def p_expr_definition_list_between(p):
-    'expr_definition_list : BETWEEN expr_definition_list'
+    'expr_definition_list : between expr_definition_list'
     p[2] = p[2].replace('\n',' ')
-    p[0] = f"{p[1].upper()} {p[2]}"
+    p[0] = f"{p[1]} {p[2]}"
 
 def p_expr_definition_list_boolean(p):
     '''
-    expr_definition_list : expr_definition AND expr_definition_list
-                         | expr_definition OR expr_definition_list
+    expr_definition_list : expr_definition and expr_definition_list
+                         | expr_definition or expr_definition_list
     '''
-    p[0] = f"{p[1]}\n{p[2].upper()} {p[3]}"
+    p[0] = f"{p[1]}\n{p[2]} {p[3]}"
 
 def p_expr_definition_value(p):
     '''
-    expr_definition : LABEL
-                    | STRING_SIMPLE
-                    | STRING_DOUBLE
-                    | STRING_GRAVE
+    expr_definition : label
+                    | string_simple
+                    | string_double
+                    | string_grave
     '''
     p[0] = p[1]
 
 def p_expr_definition_keyword(p):
     '''
-    expr_definition : DISTINCT
-                    | ALL
-                    | STAR
-                    | NULL
-                    | TRUE
-                    | FALSE
-                    | COALESCE
-                    | CAST
-                    | CONCAT
-                    | ASC
-                    | DESC
+    expr_definition : distinct
+                    | all
+                    | star
+                    | null
+                    | true
+                    | false
+                    | coalesce
+                    | cast
+                    | concat
+                    | asc
+                    | desc
     '''
-    p[0] = p[1].upper()
+    p[0] = p[1]
 
 def p_expr_definition_block(p):
     '''
     expr_definition : case_when
-                    | subquerry
+                    | select_full
     '''
     p[0] = p[1]
 
 def p_expr_definition_brackets(p):
-    'expr_definition : LEFT_BRA expr_list RIGHT_BRA'
+    'expr_definition : left_bra expr_list right_bra'
     p[0] = f"{p[1]}{p[2]}{p[3]}"
 
 def p_expr_definition_parentheses(p):
-    'expr_definition : LEFT_PAR expr_list RIGHT_PAR'
+    'expr_definition : left_par expr_list right_par'
     if is_numeric_expression(p[2]):
         p[2] = p[2].replace('\n',' ')
         p[0] = f"{p[1]}{p[2]}{p[3]}"
@@ -359,7 +401,7 @@ def p_expr_definition_parentheses(p):
         
 
 def p_expr_list_next(p):
-    'expr_list : expr_definition_list COMMA expr_list'
+    'expr_list : expr_definition_list comma expr_list'
     p[0] = f"{p[1]}{p[2]}\n{p[3]}"
 
 def p_expr_list_end(p):
@@ -368,29 +410,184 @@ def p_expr_list_end(p):
 
 # ============================================================
 
+def p_token_to_upper(p):
+    '''
+    select : SELECT
+    all : ALL
+    distinct : DISTINCT
+    from : FROM
+    where : WHERE
+    by : BY
+    group : GROUP
+    order : ORDER
+    cluster : CLUSTER
+    distribute : DISTRIBUTE
+    sort : SORT
+    having : HAVING
+    limit : LIMIT
+    as : AS
+    case : CASE
+    when : WHEN
+    then : THEN
+    else : ELSE
+    end : END
+    or : OR
+    and : AND
+    not : NOT
+    is : IS
+    in : IN
+    true : TRUE
+    false : FALSE
+    null : NULL
+    coalesce : COALESCE
+    cast : CAST
+    concat : CONCAT
+    union : UNION
+    except : EXCEPT
+    between : BETWEEN
+    asc : ASC
+    desc : DESC
+    join : JOIN
+    on : ON
+    inner : INNER
+    outer : OUTER
+    left : LEFT
+    right : RIGHT
+    full : FULL
+    semi : SEMI
+    cross : CROSS
+    natural : NATURAL
+    '''
+    p[0] = p[1].upper()
+
+def p_token_unchanged(p):
+    '''
+    comma : COMMA
+    comparison : COMPARISON
+    star : STAR
+    symbol : SYMBOL
+    semicolon : SEMICOLON
+    label : LABEL
+    left_par : LEFT_PAR
+    right_par : RIGHT_PAR
+    left_bra : LEFT_BRA
+    right_bra : RIGHT_BRA
+    string_simple : STRING_SIMPLE
+    string_double : STRING_DOUBLE
+    string_grave : STRING_GRAVE
+    '''
+    p[0] = p[1]
+
+def p_token_commented(p):
+    '''
+    select : select comment
+    all : all comment
+    distinct : distinct comment
+    from : from comment
+    where : where comment
+    by : by comment
+    group : group comment
+    order : order comment
+    cluster : cluster comment
+    distribute : distribute comment
+    sort : sort comment
+    having : having comment
+    limit : limit comment
+    as : as comment
+    case : case comment
+    when : when comment
+    then : then comment
+    else : else comment
+    end : end comment
+    or : or comment
+    and : and comment
+    not : not comment
+    is : is comment
+    in : in comment
+    true : true comment
+    false : false comment
+    null : null comment
+    coalesce : coalesce comment
+    cast : cast comment
+    concat : concat comment
+    union : union comment
+    except : except comment
+    between : between comment
+    asc : asc comment
+    desc : desc comment
+    join : join comment
+    on : on comment
+    inner : inner comment
+    outer : outer comment
+    left : left comment
+    right : right comment
+    full : full comment
+    semi : semi comment
+    cross : cross comment
+    natural : natural comment
+    comma : comma comment
+    comparison : comparison comment
+    star : star comment
+    symbol : symbol comment
+    semicolon : semicolon comment
+    label : label comment
+    left_par : left_par comment
+    right_par : right_par comment
+    left_bra : left_bra comment
+    right_bra : right_bra comment
+    string_simple : string_simple comment
+    string_double : string_double comment
+    string_grave : string_grave comment
+    '''
+    p[0] = f"{p[1]}{p[2]}"
+
+def p_comment(p):
+    'comment : COMMENT'
+    p[0] = f" {p[1]}\n"
+
+def p_comment_alone(p):
+    'comment : COMMENT_ALONE'
+    p[0] = f"\n{p[1]}\n"
+
+# ============================================================
+
+
+
 def p_error(p):
     print("Syntax error at '%s'" % p.value)
 
 yacc.yacc()
 
 query = """
+-- test commentaires tout en haut
+
 (select distinct
   case when x != 0 then 1 else case when x > 0 then 2 else 3 end end as toto_le_zigoto
 , 
 (f.f1 + '-' + f.f2)
 , f.id in (1 ,2 ,42),
-'(' + 'oui' + '`@)' + @ + 1 + (1 * 1) "select" carapuce
+'(' + 'oui' + '`@)' + @ + 1 + (1 * 1) "select" carapuce --test
 , F.je_suis_un_champ_avec_un_${uuid}, coalesce(null,null,f.champ1, f.champ2), ma_function(hex(f.est_true, b.est_true))[ 0 ]
 where (m = true or m = false) and m is null and ((1 != 0) or (m.is_false or case when 1=1 or 2=2 or case when 7=7 then ahah end then true else false end)) and o and i and j or a
 where (m = true or m = false) and m is null and ((1 != 0) or (m.is_false or case when 1=1 or 2=2 or case when 7=7 then ahah end then true else false end)) and o and i and j or a
 from (select aa from (select * from (select * from non where (1 is 1)))) where 1=1 AND 2=2
+--slt les copains
 order by 1 asc and 2 desc limit 15
-inner natural left join (select * from oui where non == non) on (2=2 and 1+1=1+1) and 1=1
+inner natural left join ( --non
+select * from oui where non == non) on (2=2 and 1+1=1+1) and 1=1
 inner natural left join ma_table)
+except( -- moui
+select * from oui)
+
+-- test commentaire tout en bas
+
+-- un deuxième
 """
 
+trim_line_regex = re.compile(r'^\s+', re.MULTILINE)
+trimmed_query = trim_line_regex.sub('', query)
 
-lex.input(query)
+lex.input(trimmed_query)
 for tok in iter(lex.token, None):
     print(repr(tok.type), repr(tok.value))
 print()
