c6fcfec00cb0cab273c7e4a5f35cd705b9940b31
floh0 <florian.dasilva@ensiie.fr>
2019-09-13 00:53:07 +0200

    change current syntax to sql if 'plain text' was selected

--- sql-formatter.py
+++ sql-formatter.py
@@ -9,14 +9,15 @@ from src import formatter
 
 class FormatQueryCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		view = self.view
-		
-		selection = view.sel()
+		selection = self.view.sel()
 		if len(selection) > 1 or not selection[0].empty():
 			regions = [region for region in selection if not(region.empty())]
 		else:
 			regions = [sublime.Region(0, self.view.size())]
 
 		for region in regions:
-			formatted_text = formatter.format_query(view.substr(region))
+			formatted_text = formatter.format_query(self.view.substr(region))
 			self.view.replace(edit, region, formatted_text)
+
+		if "Plain text" in self.view.settings().get('syntax'):
+			self.view.set_syntax_file("Packages/SQL/SQL.sublime-syntax")
